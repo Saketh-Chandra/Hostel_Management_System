dce993d0fdd609f3ab49571103783f927a38659a
Saketh Chandra <b.sakethchandra9@gmail.com>
2020-10-23 18:56:18 +0530

    edded accounts app

--- accounts/form.py
+++ accounts/form.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
+    def clean_email(self):
+        demail = self.cleaned_data['email']
+        if "amrita.edu" not in demail:
+            raise forms.ValidationError("You must be use collage Email ID")
+        return demail
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
--- accounts/views.py
+++ accounts/views.py
@@ -3,28 +3,34 @@ from django.http import HttpResponse
 # from django.contrib.auth.forms import UserCreationForm
 from .form import CustomUserCreationForm
 from django.contrib import messages
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 from .decorators import unauthenticated_user
+from django.contrib.auth.models import Group
+import re
+
 
 # Create your views here.
 
 def hello_world(request):
     return HttpResponse(f"<h3>Hello {request.user}!</h3>")
 
+
 @unauthenticated_user
 def login_views(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(username=username,password=password)
+        user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            #return redirect('user_page')
+            # return redirect('user_page')
             return HttpResponse(f'hello {request.user}')
         else:
             messages.info(request, "username or password is incorrect")
-    context={}
-    return render(request, 'accounts/loginPage.html',context)
+    context = {}
+    return render(request, 'accounts/loginPage.html', context)
+
+
 @unauthenticated_user
 def register_views(request):
     form = CustomUserCreationForm()
@@ -32,7 +38,26 @@ def register_views(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            # form.save()
+            user = form.save(commit=False)
+            user.save()
+            user_email = form.cleaned_data.get('email')
+            students = re.findall('students', user_email)
+            warden = re.findall('warden', user_email)
+            print(user_email, type(user_email), '--------------------------')
+            print('students' in students)
+            if 'students' in students:
+                # print('hello student')
+                user_group = Group.objects.get(name='student')
+                user.groups.add(user_group)
+            elif 'warden' in warden:
+                # print('hello warden')
+                user_group = Group.objects.get(name='warden')
+                user.groups.add(user_group)
+            else:
+                # print('error')
+                messages.error(request, 'its not a correct email!')
+                return redirect('register_page')
             user_name = form.cleaned_data.get('username')
             # print(user_name)
             messages.success(request, 'Account was successfully created for ' + user_name)
