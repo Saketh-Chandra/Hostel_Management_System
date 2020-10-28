d2f4b246865dd9264a85e1b7ed3ecf8158430d25
Saketh Chandra <b.sakethchandra9@gmail.com>
2020-10-28 18:07:19 +0530

    basic accounts and templates are done!

--- Hostel_Room_Booking/urls.py
+++ Hostel_Room_Booking/urls.py
@@ -18,5 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/',include('accounts.url'))
+    path('accounts/',include('accounts.url')),
+    path('',include('hostelapp.url'))
+    
 ]
--- hostelapp/views.py
+++ hostelapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'hostelapp/home.html')
+
+def sam(request):
+    return render(request,'hostelapp/sample.html')
+
+def floor(request):
+    return render(request,'hostelapp/floor.html')
+
+def rooms(request):
+    return render(request,'hostelapp/rooms.html')
+
+def student(request):
+    return render(request, 'hostelapp/student.html')
+
+def warden(request):
+    return render(request, 'hostelapp/warden.html')
+
