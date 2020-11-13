128e1b4b49970898e018185d7a3139eee5a14b87
Saketh Chandra <b.sakethchandra9@gmail.com>
2020-11-13 11:47:10 +0530

    edited hotalapp/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class CustomUser(AbstractUser):
-    pass
+# class CustomUser(AbstractUser):
+#     pass
