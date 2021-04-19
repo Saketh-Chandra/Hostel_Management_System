e42ce6c89314fab4b01aaae829f25423f6a1f95e
Dara-Ekanth <daraekanth3@gmail.com>
2021-04-19 15:11:49 +0530

    date to choose form is added.

--- Wardenapp/forms.py
+++ Wardenapp/forms.py
@@ -3,6 +3,7 @@ from django.forms import ModelForm
 from hostelapp.models import room
 from django.contrib.auth.forms import PasswordChangeForm
 from accounts.models import CustomUser
+from hostelapp.models import attendence_date,students_attendence
 
 class hidden_room_form(forms.ModelForm):
     class Meta:
@@ -16,3 +17,16 @@ class passwordchangingform(PasswordChangeForm):
     class Meta:
         model = CustomUser
         fields = ['old_password','new_password1','new_password2']
+
+class attendence_date_form(forms.Form):
+    datetaken = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
+    class Meta:
+        model = attendence_date
+        fields = ['datetaken']
+        exclude = ['warden']
+
+class attendance_form(forms.ModelForm):
+    class Meta:
+        model = students_attendence
+        fields = ['present']
+        #exclude = ['student_name','room_num','date']
